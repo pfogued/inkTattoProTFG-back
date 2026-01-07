27028fbbb1607ea62b9131023c6bd1fbdfd4c2d2
pfogued <pablitocalamocha05@gmail.com>
2026-01-07 23:46:03 +0100

    ultimo (espero)

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,7 @@ return [
         'http://127.0.0.1:8080',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://shuttle.proxy.rlwy.net:29298',
     ],
     
     'allowed_origins_patterns' => [],
